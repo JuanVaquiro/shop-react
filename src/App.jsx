60caddaf883f04d9efe4310c8a5208b6a2dc168f
JuanVaquiro <jvaquiro23@itfip.edu.co>
2023-04-01 13:10:59 -0500

    react-rounter-dom v10-6

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './styles/global.scss'
+import ErrorPage from './pages/Error-page'
 import Layout from './containers/Layout'
 import Login from './containers/login'
+import Home from './pages/Home'
+import RecoveryPassword from './containers/RecoveryPassword'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: 'login',
+    element: <Login />
+  },
+  {
+    path: 'recovery-pssword',
+    element: <RecoveryPassword />
+  }
+])
 
 function App () {
   return (
     <Layout>
-      <Login />
+      <RouterProvider router={router} />
     </Layout>
   )
 }
