528f892ed7b4ce3479f873a746583b87538fb60d
JuanVaquiro <jvaquiro23@itfip.edu.co>
2023-04-01 11:59:48 -0500

    eslint confing and structu component

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,11 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
+import Layout from './containers/Layout'
+import Login from './containers/login'
 
-function App() {
-  const [count, setCount] = useState(0) 
-
+function App () {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      asddd
-    </div>
+    <Layout>
+      <Login />
+    </Layout>
   )
 }
 
