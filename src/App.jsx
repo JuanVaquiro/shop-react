156988e786503c0c1c2dae5d98512cdf7d92b5f1
JuanVaquiro <jvaquiro23@itfip.edu.co>
2023-04-01 16:17:45 -0500

    createRoutesFromChildren

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,11 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './styles/global.scss'
-import ErrorPage from './pages/Error-page'
 import Layout from './containers/Layout'
-import Login from './containers/login'
-import Home from './pages/Home'
-import RecoveryPassword from './containers/RecoveryPassword'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: 'login',
-    element: <Login />
-  },
-  {
-    path: 'recovery-pssword',
-    element: <RecoveryPassword />
-  }
-])
+import { Outlet } from 'react-router-dom'
+import './styles/global.scss'
 
 function App () {
   return (
     <Layout>
-      <RouterProvider router={router} />
+      <Outlet />
     </Layout>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,48 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App'
+import Login from './containers/login'
+import RecoveryPassword from './containers/RecoveryPassword'
+import Home from './pages/Home'
+import ErrorPage from './pages/Error-page'
 import './styles/global.scss'
+
+// You can do this: create your routes as JSX instead of objects.
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path="/" element={<App />}>
+//       <Route path="/" element={<Home />} />
+//       <Route path="login" element={<login />} />
+//       <Route path="recovery-password" element={<RecoveryPassword />} />
+//     </Route>
+//   )
+// );
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: 'login',
+        element: <Login />
+      },
+      {
+        path: 'recovery-password',
+        element: <RecoveryPassword />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
